7057045701684385b599a69ba2a2741c8e89e074
latheanh <theanh030802@gmail.com>
2023-05-22 14:06:34 +0700

    hieuguibai

---
+++ hieu/frmDMChatlieu.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frmDMChatlieu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaChatLieu = new System.Windows.Forms.TextBox();
+            this.txtTenChatLieu = new System.Windows.Forms.TextBox();
+            this.dgvChatLieu = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChatLieu)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(17, 16);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(120, 51);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(194, 17);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(119, 48);
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(378, 23);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(129, 45);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(572, 25);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(128, 43);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.Location = new System.Drawing.Point(764, 22);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(121, 43);
+            this.btnBoQua.TabIndex = 4;
+            this.btnBoQua.Text = "Bỏ qua";
+            this.btnBoQua.UseVisualStyleBackColor = true;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(943, 21);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(109, 43);
+            this.btnDong.TabIndex = 5;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(497, 51);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "DANH MỤC LOẠI HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Mã loại hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tên loại hàng";
+            // 
+            // txtMaChatLieu
+            // 
+            this.txtMaChatLieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaChatLieu.Location = new System.Drawing.Point(321, 106);
+            this.txtMaChatLieu.Name = "txtMaChatLieu";
+            this.txtMaChatLieu.Size = new System.Drawing.Size(369, 41);
+            this.txtMaChatLieu.TabIndex = 9;
+            this.txtMaChatLieu.TextChanged += new System.EventHandler(this.txtMaChatLieu_TextChanged);
+            // 
+            // txtTenChatLieu
+            // 
+            this.txtTenChatLieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenChatLieu.Location = new System.Drawing.Point(321, 183);
+            this.txtTenChatLieu.Name = "txtTenChatLieu";
+            this.txtTenChatLieu.Size = new System.Drawing.Size(369, 41);
+            this.txtTenChatLieu.TabIndex = 10;
+            this.txtTenChatLieu.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dgvChatLieu
+            // 
+            this.dgvChatLieu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChatLieu.Location = new System.Drawing.Point(56, 265);
+            this.dgvChatLieu.Name = "dgvChatLieu";
+            this.dgvChatLieu.RowHeadersWidth = 51;
+            this.dgvChatLieu.RowTemplate.Height = 24;
+            this.dgvChatLieu.Size = new System.Drawing.Size(1006, 368);
+            this.dgvChatLieu.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDong);
+            this.panel1.Controls.Add(this.btnBoQua);
+            this.panel1.Controls.Add(this.btnLuu);
+            this.panel1.Controls.Add(this.btnSua);
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnThem);
+            this.panel1.Location = new System.Drawing.Point(11, 655);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1069, 85);
+            this.panel1.TabIndex = 12;
+            // 
+            // frmDMChatlieu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 779);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvChatLieu);
+            this.Controls.Add(this.txtTenChatLieu);
+            this.Controls.Add(this.txtMaChatLieu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDMChatlieu";
+            this.Text = "Danh mục loại hàng";
+            this.Load += new System.EventHandler(this.frmDMChatlieu_Load);
+            this.Click += new System.EventHandler(this.frmDMChatlieu_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChatLieu)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnBoQua;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaChatLieu;
+        private System.Windows.Forms.TextBox txtTenChatLieu;
+        private System.Windows.Forms.DataGridView dgvChatLieu;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
